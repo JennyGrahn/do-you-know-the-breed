d21509032c2a6b5a1661a645d0e865440e8bcc95
JennyGrahn <hovforsedd-fyrar-0k@icloud.com>
2024-03-18 17:59:02 +0100

    Change place for the array in quiz.js

--- quiz.js
+++ quiz.js
@@ -1,35 +1,6 @@
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
-const nextButton = document.getElementById('next');
-
-function buildQuiz() {
-    const output = [];
-
-    breedQuestions.forEach((currentQuestion, questionNumber) => {
-        const answers = [];
-
-        for (letter in currentQuestion.answers) {
-            answers.push(
-                `<label>
-                  <input type="radio" name="question${questionNumber}" value="${letter}">
-                  ${letter} :
-                  ${currentQuestion.answers[letter]}
-                </label>`
-            );
-        }
-
-        output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-             <div class="answers"> ${answers.join('')} </div>`
-        );
-    });
-
-    quizContainer.innerHTML = output.join('');
-}
-
-buildQuiz();
-
-nextButton.addEventListener('click', showResults);
+const nextButton = document.getElementById('next-btn');
 
 const breedQuestions = [{
         //Correct answer is Labrador retriever 
@@ -244,6 +215,35 @@ const breedQuestions = [{
         correctAnswer: "a"
     },
 ];
+
+function buildQuiz() {
+    const output = [];
+
+    breedQuestions.forEach((currentQuestion, questionNumber) => {
+        const answers = [];
+
+        for (letter in currentQuestion.answers) {
+            answers.push(
+                `<label>
+                  <input type="radio" name="question${questionNumber}" value="${letter}">
+                  ${letter} :
+                  ${currentQuestion.answers[letter]}
+                </label>`
+            );
+        }
+
+        output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+             <div class="answers"> ${answers.join('')} </div>`
+        );
+    });
+
+    quizContainer.innerHTML = output.join('');
+}
+
+buildQuiz();
+
+nextButton.addEventListener('click', showResults);
 
 function showResults() {
     const answerContainers = quizContainer.querySelectorAll('.answers');
