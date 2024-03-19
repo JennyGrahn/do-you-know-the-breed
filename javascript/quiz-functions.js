ab6743d44e2cbc1427890ecd2ef343553f7c0a7f
JennyGrahn <hovforsedd-fyrar-0k@icloud.com>
2024-03-19 23:45:00 +0100

    Remove your-results.html as it will not be needed

--- javascript/quiz-functions.js
+++ javascript/quiz-functions.js
@@ -1,108 +1,62 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const nextButton = document.getElementById('next-btn');
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
 
 let questionNumber = 0;
 let currentQuestion;
 let numCorrect = 0;
+let userAnswerSelector;
 
 function buildQuiz() {
     currentQuestion = breedQuestions[questionNumber];
     const output = [];
 
     const answers = [];
+    output.push(`<img src="breed-images/img${questionNumber}.jpg"/>`);
+    output.push(`<div class="question"> ${currentQuestion.question} </div>`);
 
-    breedQuestions.forEach((currentQuestion, questionNumber) => {
-
-
-                for (letter in currentQuestion.answers) {
-                    answers.push(
-                        `<label>
-                  <input type="radio" name="question${questionNumber}" value="${letter}">
+    for (letter in currentQuestion.answers) {
+        output.push(
+            `<div class="answer-btn answer-grid"> <label>
+                  <input type="radio" name="question${questionNumber}" value="${letter}" onclick='showAnswer()'>
                   ${letter} :
                   ${currentQuestion.answers[letter]}
-                </label>`
-                    );
-                }
+                </label> </div>`
+        );
+    }
+    output.push(`<button id="next-btn"  disabled>Next</button>`);
 
-                output.push(
-                    `<div class="question"> ${currentQuestion.question} </div>
-             <div class="answer-btn answer-grid"> ${answers.join('')} </div>
-             <button id="next-btn">Next</button>`
-                );
+    quizContainer.innerHTML = output.join("");
+    const nextButton = document.getElementById("next-btn");
+    nextButton.addEventListener("click", nextBtnHandler);
+}
 
-                quizContainer.innerHTML = output.join('');
-                nextButton.addEventListener("click", nextBtnHandler);
-            }
+//Next button to generate the next question
+const nextBtnHandler = function () {
+    questionNumber++;
+    if (questionNumber < breedQuestions.length) {
+        if (userAnswerSelector.value === currentQuestion.correctAnswer)
+            numCorrect++;
 
-            //Next button to generate the next question
-            const nextBtnHandler = function () {
-                questionNumber++;
-                if (questionNumber < breedQuestions.length) {
-                    console.log(questionNumber);
-                } else {}
-            };
+        buildQuiz();
+    } else {
+        quizContainer.innerHTML = "";
+        let result = document.getElementById("results");
+        result.innerHTML = ` you have ${numCorrect} correct answers`;
+    }
+};
 
+const showAnswer = function () {
+    document.getElementById("next-btn").disabled = false;
+    document
+        .querySelectorAll(`input[name=question${questionNumber}]`)
+        .forEach((element) => (element.parentElement.style.color = "black"));
 
-            const showAnswer = function () {
-                document
-                    .querySelectorAll(`input[name[name=question${questionNumber}]`)
-                    .forEach((element) =>
-                        element.parentElement.style.color = '#4CAF50';
-                    );
-            }
+    const selector = `input[name=question${questionNumber}]:checked`;
+    userAnswerSelector = document.querySelector(selector) || {};
 
-            const selector = `input[name=question${questionNumber}]: checked`;
-            const userAnswerSeelector = document.querySelector(selector) || {};
-
-
-            if (userAnswerSelector.value === currentQuestion.correctAnswer) {
-                numCorrect++;
-                userAnswerSelector.parentElement.style.color = 'lightgreen';
-            } else {
-                userAnswerSelector.parentElement.style.color = 'red';
-            }
-
-
-
-
-            /*const answerButtons = document.querySelectorAll('.answer-btn');
-            answerButtons.forEach((button, index) => {
-                button.addEventListener('click', () => {
-                    const currentQuestion = breedQuestions[index];
-                    const userAnswer = button.getAttribute('value');
-                    if (userAnswer === currentQuestion.correctAnswer) {
-                        button.classList.add('correct');
-                    } else {
-                        button.classList.add('wrong');
-                    }
-                });
-            });
-
-
-            /*
-                buildQuiz();
-
-                nextButton.addEventListener('click', showResults);
-
-
-                function showResults() {
-                    const answerContainers = quizContainer.querySelectorAll('.answers');
-                    let numCorrect = 0;
-
-                    breedQuestions.forEach((currentQuestion, questionNumber) => {
-                        const answerContainer = answerContainers[questionNumber];
-                        const selector = `input[name=question${questionNumber}]:checked`;
-                        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-                        if (userAnswer === currentQuestion.correctAnswer) {
-                            numCorrect++;
-                            answerContainer.style.color = 'lightgreen';
-                        } else {
-                            answerContainer.style.color = 'red';
-                        }
-                    });
-
-                    resultsContainer.innerHTML = `${numCorrect} out of ${breedQuestions.length}`;
-                }
-                */
+    if (userAnswerSelector.value === currentQuestion.correctAnswer) {
+        userAnswerSelector.parentElement.style.color = "lightgreen";
+    } else {
+        userAnswerSelector.parentElement.style.color = "red";
+    }
+};
